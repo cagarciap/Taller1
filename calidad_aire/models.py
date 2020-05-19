e41867d1384c7bddbf29483edf7a767ae00d81ba
Cesar Garcia <cagarciap@eafit.edu.co>
2020-05-18 20:28:16 -0500

    Agregar Varios Datos

--- calidad_aire/models.py
+++ calidad_aire/models.py
@@ -5,6 +5,11 @@ class calidad_aire(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(verbose_name='Tipo', max_length=20)
     value = models.IntegerField(verbose_name='Valor')
+    codigo = models.CharField(max_length=20, verbose_name='Codigo', default="x")
+    latitud = models.FloatField(verbose_name='Latitud', default=0)
+    longitud = models.FloatField(verbose_name='Longitud', default=0)
+    producto = models.CharField(verbose_name='Producto', max_length=20, default="x")
+    area = models.FloatField(verbose_name='Area', default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
